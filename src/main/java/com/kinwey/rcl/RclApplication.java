5dc35949254533897ab4e3e1cf2c78034d49fe12
里乐 <jeffly.jw@alibaba-inc.com>
2019-03-28 15:26:18 +0800

    init project

---
+++ src/main/java/com/kinwey/rcl/RclApplication.java
@@ -0,0 +1,13 @@
+package com.kinwey.rcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RclApplication.class, args);
+    }
+
+}
